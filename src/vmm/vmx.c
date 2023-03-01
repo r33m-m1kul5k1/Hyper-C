d2afa13dc446fdaa83419954d8af463be9e67370
programmer1024r <reempro112@gmail.com>
2023-03-01 23:08:44 +0200

    added cr0 and cr4 mods, need smx and region allocation

--- src/vmm/vmx.c
+++ src/vmm/vmx.c
@@ -5,18 +5,20 @@
 
 #define CR4_VMX_ENABLE (1 << 13)
 #define CR0_NE_ENABLE (1 << 5)
+
+// FIXED0 -> fixed ones
 #define IA32_VMX_CR0_FIXED0 0x486
 #define IA32_VMX_CR0_FIXED1 0x487
 #define IA32_VMX_CR4_FIXED0 0x488
 #define IA32_VMX_CR4_FIXED1 0x489
+
 #define IA32_VMX_BASIC 0x480
 #define IA32_FEATURE_CONTROL 0x3a
 
 void enter_vmx_root() {
+
     
-    qword_t value = read_msr(IA32_VMX_CR4_FIXED1);
-    char test[] = "sup";
-    memset((test+1), 0, 2);
-    log_info("test: %s", test);
-    log_info("IA32_VMX_CR4_FIXED1 value: %x", value);
+    // Note that PE & PG should be 0 with "unrestricted guest".
+    write_cr0((read_cr0() | CR0_NE_ENABLE | read_msr(IA32_VMX_CR0_FIXED0)) & read_msr(IA32_VMX_CR0_FIXED1));
+    write_cr4((read_cr4() | CR4_VMX_ENABLE | read_msr(IA32_VMX_CR4_FIXED0)) & read_msr(IA32_VMX_CR4_FIXED1));
 }
